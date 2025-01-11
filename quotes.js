eb857983b1a9a976080d034d34513ab479d346bf
dilip adhikari <105116567+dilip-adhikari01@users.noreply.github.com>
2025-01-11 22:30:27 +0530

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,17 @@
+const quotes = [
+            {
+                topic: 'प्रेरणा (Inspiration)',
+                quote: '"उद्योगेनैव कार्याणि सिध्यन्ति न मनोरथैः ।"',
+                translation: '"The key to success is effort."'
+            },
+            {
+                topic: 'ज्ञानम् (Knowledge)',
+                quote: '"विद्याधनं सर्वधनप्रधानम्"',
+                translation: '"Knowledge is the greatest wealth."'
+            },
+            {
+                topic: 'सहनशीलता (Patience)',
+                quote: '" धैर्येण धीरा धुरमारुहन्ति ।"',
+                translation: '"Patient and courageous individuals ascend to the peak."'
+            }
+        ];
